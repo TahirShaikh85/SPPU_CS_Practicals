594ef8be5eb33aa85270680c791c5926e939b968
Tahir <tahirshaikhrs7@gmail.com>
2023-01-02 22:42:34 +0530

    OOP P6

--- CG practicals/revision.cpp
+++ CG practicals/revision.cpp
@@ -1,29 +1,67 @@
-#include <fstream>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 
+struct PersonalRecord {
+  string name;
+  string dob;
+  string phone;
+};
+
+// Function for comparing two records by name
+bool compareByName(const PersonalRecord &a, const PersonalRecord &b) {
+  return a.name < b.name;
+}
+
+// Function for comparing two records by date of birth
+bool compareByDOB(const PersonalRecord &a, const PersonalRecord &b) {
+  return a.dob < b.dob;
+}
+
+// Function for comparing two records by phone number
+bool compareByPhone(const PersonalRecord &a, const PersonalRecord &b) {
+  return a.phone < b.phone;
+}
+
 int main() {
-  // Create an output file stream and open the file "output.txt" for writing
-  ofstream out("output.txt");
+  // Create a vector of personal records
+  vector<PersonalRecord> records;
 
-  // Write some information to the file
-  out << "This is a line of text" << endl;
-  out << "This is another line of text" << endl;
+  // Add some records to the vector // mm-dd-yyyy
+  records.push_back({"Tahir Shaikh", "02/03/2003", "123-456-7890"});
+  records.push_back({"Tony Stark", "03/20/1998", "234-567-8901"});
+  records.push_back({"Ashish Kandekar", "07/04/1997", "345-678-9012"});
+  records.push_back({"Abhishekh Varpe", "12/25/1999", "456-789-0123"});
+  records.push_back({"Aditya Waghmare", "01/01/2000", "456-789-0123"});
+  
+  // Sort the records by name
+  sort(records.begin(), records.end(), compareByName);
 
-  // Close the output file stream
-  out.close();
-
-  // Create an input file stream and open the file "output.txt" for reading
-  ifstream in("output.txt");
-
-  // Read the information from the file and print it to the console
-  string line;
-  while (getline(in, line)) {
-    cout << line << endl;
+  // Print the records
+  cout << "Records sorted by name:" << endl;
+  for (vector<PersonalRecord>::const_iterator it = records.begin(); it != records.end(); ++it) {
+    cout << it->name << " " << it->dob << " " << it->phone << endl;
   }
 
-  // Close the input file stream
-  in.close();
+  // Sort the records by date of birth
+  sort(records.begin(), records.end(), compareByDOB);
+
+  // Print the records
+  cout << "\nRecords sorted by date of birth:" << endl;
+  for (vector<PersonalRecord>::const_iterator it = records.begin(); it != records.end(); ++it) {
+    cout << it->name << " " << it->dob << " " << it->phone << endl;
+  }
+
+  // Sort the records by phone number
+  sort(records.begin(), records.end(), compareByPhone);
+
+  // Print the records
+  cout << "\nRecords sorted by phone number:" << endl;
+  for (vector<PersonalRecord>::const_iterator it = records.begin(); it != records.end(); ++it) {
+    cout << it->name << " " << it->dob << " " << it->phone << endl;
+  }
 
   return 0;
 }
--- OOP Practicals/OOP P6.cpp
+++ OOP Practicals/OOP P6.cpp
@@ -3,157 +3,70 @@ Write C++ program using STL for Sorting and searching with user-defined records 
 Person Record (Name, birth date, telephone no), item record (item code, item name, quantity
 and cost)
 */
-#include <iostream>    //standard input output stream header file
-#include <algorithm>    //The STL algorithms are generic because they can operate on a variety of data structures
-#include <vector>    //The header file for the STL vector library is vector.
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 
-class Item       
-{
-      public:   
-        char name[10];
-        int quantity;
-        int cost;
-        int code;
-
-        bool operator==(const Item& i1)    //Boolean operators allow you to create more complex conditional statements
-    {
-        if(code==i1.code)    //operator will return 1 if the comparison is true, or 0 if the comparison is false
-        return 1;
-        return 0;
-    }
-
-    bool operator<(const Item& i1)
-    {
-        if(code<i1.code)    //operator will return 1 if the comparison is true, or 0 if the comparison is false
-        return 1;
-        return 0;
-    }
-    
-
+struct PersonalRecord {
+  string name;
+  string dob;
+  string phone;
 };
 
-vector<Item> o1;
-void print(Item &i1);
-void display();
-void insert();
-void search();
-void dlt();
-
-bool compare(const Item &i1, const Item &i2)
-{
-   //if (i1.name != i2.name) return i1.cost < i2.cost;
-   return i1.cost < i2.cost;
+// Function for comparing two records by name
+bool compareByName(const PersonalRecord &a, const PersonalRecord &b) {
+  return a.name < b.name;
 }
 
-int main()
-{
-    int ch;
-      do
-    {  
-        cout<<"\n***** Menu *****";
-        cout<<"\n1.Insert";
-        cout<<"\n2.Display";
-        cout<<"\n3.Search";
-        cout<<"\n4.Sort";
-        cout<<"\n5.Delete";
-        cout<<"\n6.Exit";
-        cout<<"\nEnter your choice:";
-        cin>>ch;
-       
-        switch(ch)
-        {
-               case 1:
-                          insert();
-                          break;
-                   
-               case 2:
-                          display();
-                          break;
-                   
-               case 3:
-                         search();
-                         break;
-                              
-               case 4:
-                        sort(o1.begin(),o1.end(),compare);
-                        cout<<"\n\n Sorted on Cost";
-                        display();
-                        break;
-                  
-               case 5:
-                       dlt();
-                        break;
-                     
-               case 6:
-                        exit(0);
-        }
-   
-    }while(ch!=7);
-
-      return 0;
+// Function for comparing two records by date of birth
+bool compareByDOB(const PersonalRecord &a, const PersonalRecord &b) {
+  return a.dob < b.dob;
 }
 
-void insert()
-{
-    Item i1;
-    cout<<"\nEnter Item Name:";
-    cin>>i1.name;
-    cout<<"\nEnter Item Quantity:";
-    cin>>i1.quantity;
-    cout<<"\nEnter Item Cost:";
-    cin>>i1.cost;
-    cout<<"\nEnter Item Code:";
-    cin>>i1.code;
-    o1.push_back(i1);
+// Function for comparing two records by phone number
+bool compareByPhone(const PersonalRecord &a, const PersonalRecord &b) {
+  return a.phone < b.phone;
 }
 
-void display()
-{
-    for_each(o1.begin(),o1.end(),print);
-}
+int main() {
+  // Create a vector of personal records
+  vector<PersonalRecord> records;
 
-void print(Item &i1)
-{
-     cout<<"\n";
-     cout<<"\nItem Name:"<<i1.name;
-     cout<<"\nItem Quantity:"<<i1.quantity;
-     cout<<"\nItem Cost:"<<i1.cost;
-     cout<<"\nItem Code:"<<i1.code;
-}
+  // Add some records to the vector // mm-dd-yyyy
+  records.push_back({"Tahir Shaikh", "02/03/2003", "123-456-7890"});
+  records.push_back({"Tony Stark", "03/20/1998", "234-567-8901"});
+  records.push_back({"Ashish Kandekar", "07/04/1997", "345-678-9012"});
+  records.push_back({"Abhishekh Varpe", "12/25/1999", "456-789-0123"});
+  records.push_back({"Aditya Waghmare", "01/01/2000", "456-789-0123"});
+  
+  // Sort the records by name
+  sort(records.begin(), records.end(), compareByName);
 
-void search()
-{
-    vector<Item>::iterator p;
-    Item i1;
-    cout<<"\nEnter Item Code to search:";
-    cin>>i1.code;
-    p=find(o1.begin(),o1.end(),i1);
-    if(p==o1.end())
-    {
-         cout<<"\nNot found.";
-    }
-    else
-    {
-         cout<<"\nFound.";
-    }
-}
+  // Print the records
+  cout << "Records sorted by name:" << endl;
+  for (vector<PersonalRecord>::const_iterator it = records.begin(); it != records.end(); ++it) {
+    cout << it->name << " " << it->dob << " " << it->phone << endl;
+  }
 
-void dlt()
-{
-     vector<Item>::iterator p;
-    Item i1;
-    cout<<"\nEnter Item Code to delete:";
-    cin>>i1.code;
-    p=find(o1.begin(),o1.end(),i1);
-    if(p==o1.end())
-    {
-         cout<<"\nNot found.";
-    }
-    else
-    {
-         o1.erase(p);
-         cout<<"\nDeleted.";
-    }
+  // Sort the records by date of birth
+  sort(records.begin(), records.end(), compareByDOB);
+
+  // Print the records
+  cout << "\nRecords sorted by date of birth:" << endl;
+  for (vector<PersonalRecord>::const_iterator it = records.begin(); it != records.end(); ++it) {
+    cout << it->name << " " << it->dob << " " << it->phone << endl;
+  }
+
+  // Sort the records by phone number
+  sort(records.begin(), records.end(), compareByPhone);
+
+  // Print the records
+  cout << "\nRecords sorted by phone number:" << endl;
+  for (vector<PersonalRecord>::const_iterator it = records.begin(); it != records.end(); ++it) {
+    cout << it->name << " " << it->dob << " " << it->phone << endl;
+  }
+
+  return 0;
 }
