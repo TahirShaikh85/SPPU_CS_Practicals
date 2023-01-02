cf34233cd0ab0c8b07f90de10f9d42cd88e82ed9
Tahir <tahirshaikhrs7@gmail.com>
2023-01-02 19:37:31 +0530

    P1 add theory

--- OOP Practicals/OOP P1.cpp
+++ OOP Practicals/OOP P1.cpp
@@ -1,3 +1,27 @@
+/* **************************** Theory ****************************
+
+1. friend function - 
+	In C++, a friend function is a function that is defined outside a class but 
+	has the right to access the private 
+	and protected members of the class in which it is declared as a friend.
+
+	Friend functions are not member functions of the class. They are normal functions 
+	that are given special access 
+	privileges to the class and its members.
+
+2. what are istream and ostream in the given code -
+	istream and ostream are classes in the C++ standard library that define 
+	input and output streams, respectively. 
+	They are used to perform input and output operations on streams of bytes, 
+	such as reading from a file or writing to a file.
+
+3. 
+	In the main function, the program creates three complex objects: c1, c2, and c3. 
+	It then prompts the user to input values for c1 and c2, and outputs the values of 
+	c1 and c2. Next, it performs addition and subtraction of c1 and c2 
+	and stores the result in c3. Finally, it outputs the value of c3.
+*/
+
 #include<iostream>
 using namespace std;
 class complex
